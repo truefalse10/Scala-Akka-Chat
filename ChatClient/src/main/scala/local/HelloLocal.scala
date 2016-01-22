454f3ef19e92212f9bb0822d82b0b1da2817d040
Martin Reiss <martin.reiss@campus.lmu.de>
2016-01-22 14:45:41 +0100

    Adds the 'login' with a chosen nickname as command line arg

--- ChatClient/src/main/scala/local/HelloLocal.scala
+++ ChatClient/src/main/scala/local/HelloLocal.scala
@@ -5,11 +5,15 @@ import Console._
 import Stream._
 import common._
 
+import scala.util.Random
+
 object Local extends App {
+
+  val nickname = args(0)
 
   implicit val system = ActorSystem("LocalSystem")
   val localActor = system.actorOf(Props[LocalActor], name = "LocalActor")  // the local actor
-  localActor ! "STARTCLIENT"
+  localActor ! Nickname(nickname)
 
   Stream.continually(Console.readLine(">>> ")).takeWhile( _ ne null) foreach { line =>
     localActor ! LocalMessage(line);
@@ -18,24 +22,31 @@ object Local extends App {
 
 case class LocalMessage(message: String)
 
+case class Nickname(message: String)
+
 
 class LocalActor extends Actor {
 
-  val user = "Sebastian"
+  var user = ""
   // create the remote actor (Akka 2.1 syntax)
   val remote = context.actorFor("akka.tcp://HelloRemoteSystem@127.0.0.1:5150/user/RemoteActor")
 
   def receive = {
-    case "STARTCLIENT" =>
-        remote ! Login(user)
-
     case LocalMessage(message) =>
         remote ! ChatMessage(user, message)
+
+    case Nickname(message) =>
+      user = if (message != null) message else Random.nextString(7)
+      remote ! Login(user)
 
     case ChatMessage(from, message) =>
         println(s"server: $message")
 
     case _ =>
         println("something unexpected")
+  }
+
+  def setNick(nickname: String) = {
+
   }
 }
